3489e0b2545f7f8f904a5a4d6aaf9e1a700e5c35
umair22 <mumair2262@gmail.com>
2025-10-02 13:29:55 +0500

    MovieCard Styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,9 +8,9 @@ const MovieCard = () => {
         <img src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/1800/BlackPanther59f24429d2605.jpg" alt="movie pooster"className='movie_poster' />
         <div className='movie_details'>
             <h3 className='movie_details_heading'>Movie Name</h3>
-            <div className='movie_date_rate'>
+            <div className='align_center movie_date_rate'>
                 <p>10-1-2025</p>
-                <p>8.0 <img src={Star} alt="rating icon" /></p>
+                <p>8.0 <img src={Star} alt="rating icon" className='card_emoji' /></p>
             </div>
             <p className='movie_description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam amet provident quos sequi ut.</p>
         </div>
